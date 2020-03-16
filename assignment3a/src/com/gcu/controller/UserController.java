9a262390680ebe3e535b9b6c1348150546571c6a
Daniel Cender <danielcender@gmail.com>
2020-03-15 20:09:05 -0500

    chore: doing week 3 stuff

---
+++ assignment3a/src/com/gcu/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.gcu.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gcu.business.OrdersBusinessInterface;
+import com.gcu.model.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	OrdersBusinessInterface service;
+	
+	@Autowired
+	public void setOrdersInterface(OrdersBusinessInterface i) {
+		service = i;
+	}
+	
+	@RequestMapping(path = "/add", method = RequestMethod.GET) 
+	public ModelAndView displayForm() {
+		return new ModelAndView("addUser", "user", new User());
+	}
+	
+	@RequestMapping(path="/adduser", method = RequestMethod.POST)
+	public ModelAndView addUser(@ModelAttribute("user") @Valid User user, BindingResult result) {
+		if(result.hasErrors()) {
+			return new ModelAndView("addUser", "user", user);
+		} else {
+			List<User> users = getUsers(user);
+			return new ModelAndView("displayUsers", "users", users);
+		}
+	}
+	
+	private List<User> getUsers(User u) {
+		List<User> users = new ArrayList<User>();
+		users.add(new User("Brian", "Stetson", 1));
+		users.add(new User("Norma", "Rivers", 1));
+		users.add(new User("Jack", "Link", 1));
+		users.add(u); // Add recently added user
+		return users;
+	}
+}
